6354bb0d9f0c03ca7fae56d77a561780cc7917bf
camargodev <camargo.dev@gmail.com>
2018-12-18 00:10:06 -0200

    Advances on d6c1

--- d6c1.py
+++ d6c1.py
@@ -19,14 +19,42 @@ with open("input", "r") as f:
 for point in points:
 	x, y = point[1]
 	maxx = max(x, maxx)
-	mayy = max(y, mayy)
+	maxy = max(y, maxy)
 
-M = [[0 for x in range(maxx+1)] for y in range(maxy+1)]
+M = [[None for x in range(maxy+2)] for y in range(maxx+2)]
 
-for i in range(maxx+1):
-	for j in range(maxy+1):
+for point in points:
+	x, y = point[1]
+	M[y][x] = point[0]
+
+for i in range(maxy+1):
+	for j in range(maxx+1):
 		closest = None
+		closest_dist = 0
+		counter = 0
 		for point in points:
+			dist = manhattan_dist((j, i), point[1])
+			#print(i, j, dist)
+			if closest == None:
+				closest = point[0]
+				closest_dist = dist
+			elif closest_dist > dist:
+				closest = point[0]
+				closest_dist = dist
+		for point in points:
+			dist = manhattan_dist((j, i), point[1])
+			if dist == closest_dist:
+				counter += 1
+		if counter > 1:
+			closest = None
+		M[i][j] = closest
+
+for i in range(maxy+1):
+	line = ''
+	for j in range(maxx+1):
+		line += ' '
+		line += '.' if M[i][j] == None else str(M[i][j])
+	print(line)
 
 
 				
